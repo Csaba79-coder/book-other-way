3295c80294efbdd529d2ec338651fb9bb27c4bfb
Csaba Vadász <csabavadasz79@gmail.com>
2024-03-27 20:02:26 +0100

    add generic website into comment

--- src/main/java/com/csaba79coder/BookApp.java
+++ src/main/java/com/csaba79coder/BookApp.java
@@ -1,11 +1,6 @@
 package com.csaba79coder;
 
 import com.csaba79coder.bootstrap.DataLoader;
-import com.csaba79coder.model.Book;
-import com.csaba79coder.service.BookService;
-import com.csaba79coder.util.FileHandler;
-
-import java.util.List;
 
 public class BookApp {
 
--- src/main/java/com/csaba79coder/util/Printer.java
+++ src/main/java/com/csaba79coder/util/Printer.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class Printer {
 
+    // https://docs.oracle.com/javase/tutorial/java/generics/types.html
+    // https://www.geeksforgeeks.org/generics-in-java/
+
     public static <T> void printList(List<T> list) {
         for (T item : list) {
             System.out.println(item);
